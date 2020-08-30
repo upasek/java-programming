bd47e7625c03a26a182c5429d51155ef1a662ed7
kiran santosh upase <kvsupase@gmail.com>
2020-08-30 08:32:25 +0530

    compare strings 4

---
+++ String/StringCompare_4.java
@@ -0,0 +1,15 @@
+// compare strings by ' == '(equals) operater.
+
+public class StringCompare_4{
+
+    public static void main(String args[]){
+        String s1 = "engineering";
+        String s2 = "engineering";
+        String s3 = new String("engi neering");
+
+        System.out.println(s1 == s2);
+        System.out.println(s2 == s3);
+    
+    }
+
+}
