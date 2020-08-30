35b22e741574290f580f6017a88f138140675c0e
kiran santosh upase <kvsupase@gmail.com>
2020-08-30 08:30:29 +0530

    compare strings 3

---
+++ String/StringCompare_3.java
@@ -0,0 +1,14 @@
+//compare strings by usigng compareTo() .
+
+public class StringCompare_3{
+
+    public static void main(String args[]){
+        Integer x =  5;
+
+        System.out.println(x.compareTo(3));
+        System.out.println(x.compareTo(5));
+        System.out.println(x.compareTo(8));
+
+    }
+
+}
