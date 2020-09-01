f38f83d568eaa358419e6180a626cbf4c62bacd5
kiran santosh upase <kvsupase@gmail.com>
2020-09-01 18:54:49 +0530

    index

---
+++ String/index.java
@@ -0,0 +1,10 @@
+public class index{
+
+    public static void main(String args[]){
+        String str = "enrgineering";
+        int indstr = str.lastIndexOf('r');
+        System.out.println("First index of 'r' is "+str.indexOf('r'));
+        System.out.println("Last index of 'r' is "+indstr);
+    }
+
+}
