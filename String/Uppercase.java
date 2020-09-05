c52abc021405fee0e974dfd919303e6e3f8390c2
kiran santosh upase <kvsupase@gmail.com>
2020-09-05 23:35:08 +0530

    upper case

---
+++ String/Uppercase.java
@@ -0,0 +1,11 @@
+//How to convert a string totally into upper case?
+
+
+public class Uppercase{
+
+    public static void main(String args[]){
+        String str = "Kiran is a bad boy";
+        System.out.println("THe string in uper case : "+str.toUpperCase());
+    }
+
+}
