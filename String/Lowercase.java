3e533d479e094eb9327adcc1df9a93dd689df9e3
kiran santosh upase <kvsupase@gmail.com>
2020-09-05 23:36:19 +0530

    lower case

---
+++ String/Lowercase.java
@@ -0,0 +1,10 @@
+//How to convert a string totally into lower case?
+
+
+public class Lowercase{
+
+    public static void main(String args[]){
+        String str = "KIRAN IS A BAD BOY";
+        System.out.println("The string in lower case : "+str.toLowerCase());
+    }
+}
